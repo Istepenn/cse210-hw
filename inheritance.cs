928cb97fab073f7b3404f57d9379016b039da9a9
Istepenn <suitonkyle@gmail.com>
2024-03-27 21:18:11 +0800

    prep 5

---
+++ inheritance.cs
@@ -0,0 +1,14 @@
+// Base class for all activities
+public class Activity
+{
+    protected string name;
+    protected int durationInSeconds;
+
+    // Constructor and methods...
+}
+
+// Child class inheriting from Activity
+public class BreathingActivity : Activity
+{
+    // Constructor and additional methods...
+}
