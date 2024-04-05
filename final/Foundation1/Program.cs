c1c79e0021f7835e3b7e2e1ea7ffd70f9bb9951b
Istepenn <suitonkyle@gmail.com>
2024-04-05 20:25:45 +0800

    prove w06

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -1,9 +1,73 @@
 using System;
+using System.Collections.Generic;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Foundation1 World!");
+class Video {
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public int LengthInSeconds { get; set; }
+    private List<Comment> comments;
+
+    public Video(string title, string author, int lengthInSeconds) {
+        Title = title;
+        Author = author;
+        LengthInSeconds = lengthInSeconds;
+        comments = new List<Comment>();
+    }
+
+    public void AddComment(Comment comment) {
+        comments.Add(comment);
+    }
+
+    public int GetNumComments() {
+        return comments.Count;
+    }
+
+    public void DisplayDetails() {
+        Console.WriteLine($"Title: {Title}");
+        Console.WriteLine($"Author: {Author}");
+        Console.WriteLine($"Length: {LengthInSeconds} seconds");
+        Console.WriteLine($"Number of Comments: {GetNumComments()}");
+        Console.WriteLine("Comments:");
+        foreach (var comment in comments) {
+            Console.WriteLine($"- {comment.Name}: {comment.Text}");
+        }
+        Console.WriteLine();
+    }
+}
+
+class Comment {
+    public string Name { get; set; }
+    public string Text { get; set; }
+
+    public Comment(string name, string text) {
+        Name = name;
+        Text = text;
+    }
+}
+
+class Program {
+    static void Main(string[] args) {
+        List<Video> videos = new List<Video>();
+
+        // Creating videos
+        Video video1 = new Video("Video Title 1", "Author 1", 180);
+        video1.AddComment(new Comment("User1", "Great video!"));
+        video1.AddComment(new Comment("User2", "Nice content!"));
+        video1.AddComment(new Comment("User3", "Thanks for sharing!"));
+        videos.Add(video1);
+
+        Video video2 = new Video("Video Title 2", "Author 2", 240);
+        video2.AddComment(new Comment("User4", "Informative video."));
+        video2.AddComment(new Comment("User5", "Keep up the good work!"));
+        videos.Add(video2);
+
+        Video video3 = new Video("Video Title 3", "Author 3", 300);
+        video3.AddComment(new Comment("User6", "Interesting topic!"));
+        videos.Add(video3);
+
+        // Displaying video details
+        foreach (var video in videos) {
+            video.DisplayDetails();
+        }
     }
 }
--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -1,9 +1,113 @@
 using System;
+using System.Collections.Generic;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Foundation2 World!");
+class Product {
+    public string Name { get; set; }
+    public int ProductId { get; set; }
+    public double Price { get; set; }
+    public int Quantity { get; set; }
+
+    public Product(string name, int productId, double price, int quantity) {
+        Name = name;
+        ProductId = productId;
+        Price = price;
+        Quantity = quantity;
+    }
+
+    public double GetTotalCost() {
+        return Price * Quantity;
+    }
+}
+
+class Address {
+    public string Street { get; set; }
+    public string City { get; set; }
+    public string State { get; set; }
+    public string Country { get; set; }
+
+    public Address(string street, string city, string state, string country) {
+        Street = street;
+        City = city;
+        State = state;
+        Country = country;
+    }
+
+    public bool IsInUSA() {
+        return Country.ToLower() == "usa";
+    }
+
+    public string FormatAddress() {
+        return $"{Street}, {City}, {State}, {Country}";
+    }
+}
+
+class Customer {
+    public string Name { get; set; }
+    public Address Address { get; set; }
+
+    public Customer(string name, Address address) {
+        Name = name;
+        Address = address;
+    }
+
+    public bool IsInUSA() {
+        return Address.IsInUSA();
+    }
+}
+
+class Order {
+    private List<Product> products;
+    public Customer Customer { get; set; }
+
+    public Order(Customer customer) {
+        Customer = customer;
+        products = new List<Product>();
+    }
+
+    public void AddProduct(Product product) {
+        products.Add(product);
+    }
+
+    public double CalculateTotalCost() {
+        double totalCost = 0;
+        foreach (var product in products) {
+            totalCost += product.GetTotalCost();
+        }
+        return totalCost + (Customer.IsInUSA() ? 5 : 35);
+    }
+
+    public string GeneratePackingLabel() {
+        string label = "Packing Label:\n";
+        foreach (var product in products) {
+            label += $"Product: {product.Name}, ID: {product.ProductId}\n";
+        }
+        return label;
+    }
+
+    public string GenerateShippingLabel() {
+        return $"Shipping Label:\n{Customer.Name}\n{Customer.Address.FormatAddress()}";
+    }
+}
+
+class Program {
+    static void Main(string[] args) {
+        Address address1 = new Address("123 Main St", "City1", "State1", "USA");
+        Customer customer1 = new Customer("Customer1", address1);
+        Order order1 = new Order(customer1);
+        order1.AddProduct(new Product("Product1", 1, 10, 2));
+        order1.AddProduct(new Product("Product2", 2, 15, 1));
+
+        Console.WriteLine(order1.GeneratePackingLabel());
+        Console.WriteLine(order1.GenerateShippingLabel());
+        Console.WriteLine($"Total Cost: ${order1.CalculateTotalCost()}");
+
+        Address address2 = new Address("456 Elm St", "City2", "State2", "Canada");
+        Customer customer2 = new Customer("Customer2", address2);
+        Order order2 = new Order(customer2);
+        order2.AddProduct(new Product("Product3", 3, 20, 3));
+
+        Console.WriteLine(order2.GeneratePackingLabel());
+        Console.WriteLine(order2.GenerateShippingLabel());
+        Console.WriteLine($"Total Cost: ${order2.CalculateTotalCost()}");
     }
 }
